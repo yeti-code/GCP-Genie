7358fe4a84cb70a54ed98b4a56ef21f3d981b277
Yeti <76129522+yeti-code@users.noreply.github.com>
2024-02-27 16:41:25 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,8 +11,10 @@ curl -OL https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
 
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 
 source ~/.profile
+source ~/.bashrc
 
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest

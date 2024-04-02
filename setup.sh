151025dc23f350d418f7a6524a97ad9935e294b6
Yeti <76129522+yeti-code@users.noreply.github.com>
2024-04-02 01:26:43 -0500

    Remove old GCP function in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,5 @@
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
-go install -v github.com/yeti-code/gcp-genie-functions@1.2.0
 go install -v github.com/projectdiscovery/notify/cmd/notify@latest
 go install -v github.com/tomnomnom/anew@latest

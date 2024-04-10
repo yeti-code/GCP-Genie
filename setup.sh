c147baf0a852dc1abe15d9bd7c565fe5e0a3455d
Yeti <76129522+yeti-code@users.noreply.github.com>
2024-04-10 11:37:58 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
-go install -v github.com/projectdiscovery/notify/cmd/notify@latest
-go install -v github.com/tomnomnom/anew@latest
+# List of Golang tools to check and install
+tools=(
+    "github.com/projectdiscovery/subfinder/v2/cmd/subfinder"
+    "github.com/projectdiscovery/httpx/cmd/httpx"
+    "github.com/projectdiscovery/nuclei/v3/cmd/nuclei"
+    "github.com/projectdiscovery/notify/cmd/notify"
+    "github.com/tomnomnom/anew"
+)
+
+for tool in "${tools[@]}"; do
+    # Extract the tool name from the path
+    tool_name=$(basename "$tool")
+
+    # Check if the tool is installed
+    if ! command -v "$tool_name" &> /dev/null; then
+        echo "$tool_name is not installed. Installing..."
+        go install -v "$tool@latest"
+    else
+        echo "$tool_name is already installed."
+    fi
+done
